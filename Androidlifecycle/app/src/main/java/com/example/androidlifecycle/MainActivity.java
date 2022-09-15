c3ec17ee66133f4b44ed7c483781740663d4d8f0
Shilna <Shilna8086@gmail.com>
2022-09-15 15:30:36 +0530

    Androidlifecycle

---
+++ Androidlifecycle/app/src/main/java/com/example/androidlifecycle/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.androidlifecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(), "onCreate Called", Toast.LENGTH_LONG).show();
+        findViewById(R.id.button).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent i=new Intent( MainActivity.this,MainActivity2.class);
+                        startActivity(i);
+                    }
+                }
+        );
+    }
+
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(getApplicationContext(), "onStart Called", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(getApplicationContext(), "onRestart Called", Toast.LENGTH_LONG).show();
+    }
+
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(getApplicationContext(), "onPause Called", Toast.LENGTH_LONG).show();
+    }
+
+    protected void onResume() {
+        super.onResume();
+        Toast toast;
+        Toast.makeText(getApplicationContext(), "onResume Called", Toast.LENGTH_LONG).show();
+    }
+
+    protected void onStop() {
+        super.onStop();
+        Toast toast;
+        Toast.makeText(getApplicationContext(), "onStop Called", Toast.LENGTH_LONG).show();
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast toast;
+        Toast.makeText(getApplicationContext(), "onDestroy Called", Toast.LENGTH_LONG).show();
+    }
+}
